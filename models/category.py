ec4815e28911ebc7211f585aa16181b3763cf100
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-28 16:53:22 -0300

    criação model category

---
+++ models/category.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, VARCHAR
+from models.base_model import BaseModel
+from sqlalchemy.orm import validates
+
+
+class Category(BaseModel):
+    __tablename__ = 'category'
+    __name = Column('name', VARCHAR(length=200), nullable=False)
+    __description = Column('description', VARCHAR(length=400), nullable=False)
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Please write a name!")
+        elif not isinstance(name, str):
+            raise TypeError("Name must be string")
+        elif name.strip(' ') == '':
+            raise ValueError("Name can't be empty!")
+        elif len(name) > 200:
+            raise ValueError("Name must contains less than 200 characters!")
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not description:
+            raise ValueError("Please write a description!")
+        elif not isinstance(description, str):
+            raise TypeError("Description must be string")
+        elif description.strip(' ') == '':
+            raise ValueError("Description can't be empty!")
+        elif len(description) > 400:
+            raise ValueError("Description must contains less than 400 characters!")
+        return description
+
---
+++ tests/models/test_category.py
@@ -0,0 +1,68 @@
+import sys
+
+sys.path.append(".")
+
+from models.category import Category
+
+
+def test_compare_column_model():
+    name = 'Category'
+    description = 'Description'
+
+    category = Category(name, description)
+    assert category.name == name
+    assert category.description == description
+
+
+def test_compare_type_column_model():
+    name = 'Category'
+    description = 'Description'
+
+    category = Category(name, description)
+    assert type(category.name) == str
+    assert type(category.description) == str
+
+
+def test_compare_isinstance():
+    name = 'Category'
+    description = 'Description'
+
+    category = Category(name, description)
+    assert isinstance(category, Category)
+
+
+def test_has_attribute():
+    name = 'Category'
+    description = 'Description'
+
+    category = Category(name, description)
+    assert hasattr(category, 'name')
+    assert hasattr(category, 'description')
+
+
+def test_none_name_should_return_valueError():
+    try:
+        Category(None, 'desc')
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_none_description_should_return_valueError():
+    try:
+        Category('name', None)
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_not_str_description_should_return_TypeError():
+    try:
+        Category('name', 1)
+    except Exception as e:
+        assert isinstance(e, TypeError)
+
+
+def test_not_str_name_should_return_TypeError():
+    try:
+        Category(2, '1')
+    except Exception as e:
+        assert isinstance(e, TypeError)
