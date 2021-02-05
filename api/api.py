f6d0c276c41c6831ac3d630ba01167498e933202
anapaula-nestor <anapaula.nestor@olist.com>
2021-02-05 11:42:08 -0300

    inclusão api

---
+++ api/api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+from resources.category_resource import CategoryResource
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(CategoryResource, '/api/category', endpoint='category')
+api.add_resource(CategoryResource, '/api/category/<int:id>', endpoint='categories')
+
+
+
+@app.route('/')
+def home():
+    return 'Teste!'
+
+
+app.run(debug=True)
